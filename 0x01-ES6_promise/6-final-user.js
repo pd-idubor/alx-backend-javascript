1245d23ebf2958798ffd553e55f13c5f37661806
Precious Idubor <preciousidubor@gmail.com>
2023-06-29 20:51:22 +0000

    All settled method

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,21 @@
+import signUser from './4-user-promise.js';
+import uploadPhoto from './5-photo-reject.js';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const arr = [];
+  const prom1 = signUser(firstName, lastName);
+  const prom2 = uploadPhoto(fileName);
+  const promises = [prom1, prom2];
+
+  return Promise.allSettled(promises)
+    .then((results) => {
+      results.forEach((result) => {
+        arr.push({
+          status: result.status,
+          value: result.status === 'fulfilled' ? result.value : String(result.reason),
+        });
+      });
+      console.log(arr);
+      return arr;
+    });
+}
