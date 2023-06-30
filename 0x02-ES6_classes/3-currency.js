679c7b68e9c1314fbaadda8afd5d3138c236cb3e
Precious Idubor <preciousidubor@gmail.com>
2023-06-30 20:51:11 +0000

    Class methods

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,32 @@
+export default class Currency {
+  constructor(code, name) {
+    if (typeof code !== 'string') {
+      throw new TypeError('Code must be a string');
+    }
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._code = code;
+    this._name = name;
+  }
+
+  set code(codeStr) {
+    this._code = codeStr;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set name(strName) {
+    this._name = strName;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  displayFullCurrency() {
+    return `${this.name} (${this.code})`;
+  }
+}
