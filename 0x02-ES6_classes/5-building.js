6bc4822c4f4832f72e3d80c592aab5cf1fee18f4
Precious Idubor <preciousidubor@gmail.com>
2023-06-30 22:14:20 +0000

    Building class impl

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,15 @@
+export default class Building {
+  constructor(sqft) {
+    if (typeof sqft !== 'number') {
+      throw new TypeError('Sqft must be a number');
+    }
+    this._sqft = sqft;
+    if (this.constructor !== Building && !this.evacuationWarningMessage) {
+      throw Error('Class extending Building must override evacuationWarningMessage');
+    }
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+}
